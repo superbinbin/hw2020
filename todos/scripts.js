bc4972df47425f47922f73db8ab543ee91f9d6b6
changbinbin <2265158771@qq.com>
2020-06-14 17:10:01 +0800

    input  基本完成

--- todos/scripts.js
+++ todos/scripts.js
@@ -1,1 +1,24 @@
-console.log("hi");
+
+let tasks = [];
+
+function renderEditor() {
+    let inputEl = document.querySelector("#default-todo-panel .todo-editor > input") ;
+
+       // inputEl.onchange =(e)=> {
+       // console.log("text, ", e.target.value);
+       //console.log(""input change: ", e");
+    //};
+    let addEl = document.querySelector("#default-todo-panel .todo-editor >  button") ;
+    addEl.onclick = (e) =>{
+        console.log("add click");
+        let newTask = {
+            title:inputEl.value,
+            done:false,
+        }; 
+
+        inputEl.value = "";
+        tasks.push(newTask);
+        console.log("tasks:",tasks);
+    };
+}
+renderEditor();
